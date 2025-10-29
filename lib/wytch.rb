866d69f48cc2f0aef40ac8c8db92e837460fd55d
Jared Norman <jared@super.gd>
2025-10-29 15:35:54 -0700

    Use Zeitwerk for loading code

--- lib/wytch.rb
+++ lib/wytch.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-require_relative "wytch/version"
-require_relative "wytch/cli"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect("cli" => "CLI")
+loader.setup
 
 module Wytch
   class Error < StandardError; end
-  # Your code goes here...
 end
+
+loader.eager_load
