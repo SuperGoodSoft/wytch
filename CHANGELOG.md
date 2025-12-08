1b926d48334f370b30d7f7f42182a529e5a584ba
Jared Norman <jared@super.gd>
2025-12-07 18:22:36 -0800

    v0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+## [0.5.0] - 2025-12-7
+
 - Documentation cleanup
 
 ## [0.4.0] - 2025-12-7
--- lib/wytch/version.rb
+++ lib/wytch/version.rb
@@ -2,5 +2,5 @@
 
 module Wytch
   # The current version of Wytch.
-  VERSION = "0.4.0"
+  VERSION = "0.5.0"
 end
